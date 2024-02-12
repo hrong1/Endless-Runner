f03d0767033b5d8fbe2e6a1c430c032f6e4f797e
hrong1 <hrong1@ucsc.edu>
2024-02-12 14:51:22 -0800

    modified player and play

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -1,3 +1,13 @@
 class Player extends Phaser.GameObjects.Sprite {
-    
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame)
+        scene.physics.add.existing(this)
+        scene.add.existing(this)
+    }
+
+    update(){
+        if(Phaser.Input.Keyboard.JustDown(keySPACE)){
+            
+        }
+    }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,7 +8,7 @@ class Play extends Phaser.Scene {
     }
 
     create(){
-
+        this.player = new Player(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0)
     }
 
     update(){
